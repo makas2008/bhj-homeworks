1137d8c29316e7a4c872c69bf30f261453719f6a
Николай <rakach2009@mail.ru>
2025-01-30 21:22:24 +0300

    Add files via upload

---
+++ event-object/keysolo/task.js
@@ -0,0 +1,86 @@
+class Game {
+  constructor(container) {
+    this.container = container;
+    this.wordElement = container.querySelector('.word');
+    this.winsElement = container.querySelector('.status__wins');
+    this.lossElement = container.querySelector('.status__loss');
+    this.reset();
+    this.registerEvents();
+  }
+
+  reset() {
+    this.setNewWord();
+    this.winsElement.textContent = 0;
+    this.lossElement.textContent = 0;
+  }
+
+  registerEvents() {
+    window.addEventListener("keyup", event => {
+      if (event.key === "Shift") return;
+      event.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase() ? this.success() : this.fail();
+    });
+  }
+
+  success() {
+    if (this.currentSymbol.classList.contains("symbol_current")) {
+      this.currentSymbol.classList.remove("symbol_current");
+    }
+    this.currentSymbol.classList.add('symbol_correct');
+    this.currentSymbol = this.currentSymbol.nextElementSibling;
+
+    if (this.currentSymbol !== null) {
+      this.currentSymbol.classList.add('symbol_current');
+      return;
+    }
+
+    if (++this.winsElement.textContent === 10) {
+      alert('Победа!');
+      this.reset();
+    }
+    this.setNewWord();
+  }
+
+  fail() {
+    if (++this.lossElement.textContent === 5) {
+      alert('Вы проиграли!');
+      this.reset();
+    }
+    this.setNewWord();
+  }
+
+  setNewWord() {
+    const word = this.getWord();
+    this.renderWord(word);
+  }
+
+  getWord() {
+    const words = [
+      'Bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ];
+    const index = Math.floor(Math.random() * words.length);
+    return words[index];
+  }
+
+  renderWord(word) {
+    const html = [...word]
+      .map(
+        (s, i) =>
+          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+      )
+      .join('');
+    this.wordElement.innerHTML = html;
+    this.currentSymbol = this.wordElement.querySelector('.symbol_current');
+  }
+}
+
+new Game(document.getElementById('game'));
