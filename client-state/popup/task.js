996a4f7deb362059551185660c0a6d54450a4b66
Николай <rakach2009@mail.ru>
2025-01-31 20:31:38 +0300

    Add files via upload

---
+++ client-state/popup/task.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const modal = document.getElementById("subscribe-modal");
+const modalClose = document.querySelector(".modal__close");
+
+if (!document.cookie.includes("close=true")) {
+  modal.classList.add("modal_active");
+}
+
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("modal_active");
+  document.cookie = `${encodeURIComponent("close")}=${encodeURIComponent("true")}`;
+});
